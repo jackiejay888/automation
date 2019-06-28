9bc32a7288e6694d37f1c4d16dd6a88e0609c1ad
ZL <ZL.Chen@advantech.com.tw>
2019-06-28 17:57:50 +0800

    Update the cv code under the test folder.

--- suite/test/cv_devicefinder.py
+++ suite/test/cv_devicefinder.py
@@ -9,6 +9,8 @@ Created on 2019/06/28
 import os
 import time
 import cv2
+import argparse
+import imutils
 import pyscreenshot
 from skimage.measure import compare_ssim
 
@@ -16,8 +18,8 @@ from skimage.measure import compare_ssim
 class cv_devicefinder(object):
 
 	def screenshot(self, screenshot):
-		self.image = pyscreenshot.grab()
-		self.image.save(screenshot)
+		image = pyscreenshot.grab()
+		image.save(screenshot)
 		pass
 
 	def compare_image(self, screenshot_1, screenshot_2):
@@ -25,38 +27,43 @@ class cv_devicefinder(object):
 		self.imageB = cv2.imread(screenshot_2)
 		self.grayA = cv2.cvtColor(self.imageA, cv2.COLOR_BGR2GRAY)
 		self.grayB = cv2.cvtColor(self.imageB, cv2.COLOR_BGR2GRAY)
-		return(self._diff())
+		self._diff()
 		self._find_diff()
 		pass
 
 	def _diff(self):
-		(score, diff) = compare_ssim(self.grayA, self.grayB, full=True)
+		(score, self.diff) = compare_ssim(self.grayA, self.grayB, full=True)
+		self.diff = (self.diff * 255).astype('uint8')
 		print('SSIM: {}'.format(score))
 		if score == 1.0:
-			return('The compare is passed.')
+			print('The screenshot is same with 1 and 2.')
 		else:
-			return('The compare is failed.')
+			print('The screenshot is different with 1 and 2.')
 		pass
 
 	def _find_diff(self):
 		thresh = cv2.threshold(
-			diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+			self.diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 		cnts = cv2.findContours(
 			thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+		cnts = cnts[0]
 		for c in cnts:
 			(x, y, w, h) = cv2.boundingRect(c)
-			cv2.rectangle(imageA, (x, y), (x+w, y+h), (0, 0, 255), 2)
-			cv2.rectangle(imageB, (x, y), (x+w, y+h), (0, 0, 255), 2)
-		cv2.imshow('Modified', imageB)
-		cv2.imwrite('haha2.jpg', imageB)
+			cv2.rectangle(self.imageA, (x, y), (x+w, y+h), (0, 0, 255), 2)
+			cv2.rectangle(self.imageB, (x, y), (x+w, y+h), (0, 0, 255), 2)
+		cv2.imwrite('compare_resulte.jpg', self.imageB)
+		cv2.imshow('compare_resulte', self.imageB)
 		cv2.waitKey(0)
+		pass
 
 
 if __name__ == '__main__':
-	# os.system('del /f /q *.jpg')
+	os.system('del /f /q *.jpg')
 	opencv = cv_devicefinder()
-	# for count in range(2):
-	# 	opencv.screenshot('screenshot_' + str(count + 1) + '.jpg')
-	# 	print('Screenshot the ' + '\'screenshot_' + str(count + 1) + '.jpg\'')
-	print(opencv.compare_image('screenshot_1.jpg', 'screenshot_2.jpg'))
+	for count in range(2):
+		for wait in range(10):
+			time.sleep(1)
+			print('Waiting...')
+		opencv.screenshot('screenshot_' + str(count + 1) + '.jpg')
+		print('Screenshot the ' + '\'screenshot_' + str(count + 1) + '.jpg\'')
+	opencv.compare_image('screenshot_1.jpg', 'screenshot_2.jpg')
