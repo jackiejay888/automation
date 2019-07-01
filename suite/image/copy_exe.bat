3c1c4deba0499ef789f971ba8144962cdafe4397
ZL <ZL.Chen@advantech.com.tw>
2019-07-01 16:44:23 +0800

    Add the cv.py code.

---
+++ suite/image/copy_exe.bat
@@ -0,0 +1,11 @@
+D:
+cd D:\code\automation\suite\image
+copy /Y dist\cv*.exe .
+del /f /q *.log
+del /f /q *.txt
+del /f /q *.spec
+del /f /q *.pyc
+rd /s /q build
+rd /s /q dist
+rd /s /q __pycache__
+del /f /q *.jpg
