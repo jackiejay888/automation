a7bcf6f838aba0a664f6e466582b51b100e0bff4
ZL Chen <ZL.Chen@advantech.com.tw>
2019-08-13 16:00:55 +0800

    Update luminance code.

--- suite/luminance_pattern/luminance.py
+++ suite/luminance_pattern/luminance.py
@@ -1,16 +1,12 @@
 '''
-Created on 2019/08/07
+Created on 2019/08/13
 
-@Title: Coding the HTML code base on the circle
+@Title: Coding the HTML code base on the circle by multi color
 @Author: ZL Chen
 '''
 
 import os
 import imgkit
-import configparser
-
-config = configparser.ConfigParser()
-config.read('luminance.config')
 
 
 class luminance(object):
@@ -19,7 +15,19 @@ class luminance(object):
 		if fill_color >= 6 or fill_color <= 0:
 			raise
 		numbers = {
-			1: 'white',
+			1: '#ffffff',
+			2: '#00ff00',
+			3: '#ff0000',
+			4: '#0000ff',
+			5: '#000000'
+		}
+		return numbers.get(fill_color, None)
+
+	def fill_color_rename(self, fill_color):
+		if fill_color >= 6 or fill_color <= 0:
+			raise
+		numbers = {
+			1: 'write',
 			2: 'green',
 			3: 'red',
 			4: 'blue',
@@ -31,36 +39,24 @@ class luminance(object):
 		if stroke_color >= 6 or stroke_color <= 0:
 			raise
 		numbers = {
-			1: 'black',
-			2: 'white',
-			3: 'white',
-			4: 'white',
-			5: 'white'
+			1: '#000000',
+			2: '#ffffff',
+			3: '#ffffff',
+			4: '#ffffff',
+			5: '#ffffff'
 		}
 		return numbers.get(stroke_color, None)
 
 	def initial(self):
 		global fill_color, stroke_color
 		try:
-			w = int(config.get('luminance', 'w'))
-			h = int(config.get('luminance', 'h'))
-			aliquots = int(config.get('luminance', 'aliquots'))
-			pixel_pitch = float(config.get('luminance', 'pixel_pitch'))
+			w = int(input('Input the width (pixel) : '))
+			h = int(input('Input the height (pixel) : '))
+			aliquots = int(input('Input the aliquots : '))
+			pixel_pitch = float(input('Input the pixel_pitch : '))
 			point = int(input('Point (1 or 5 or 9) : '))
-
-			fill_color = int(input(
-				'Input the background color. (1 or 2 or 3 or 4 or 5)\n1.white 2.green 3.red 4.blue 5.black: '))
-			solid_radius = float(config.get('luminance', 'radius')) / pixel_pitch
+			solid_radius = 22.5 / pixel_pitch
 			ask_edge_point = input('Edge point (y/n) ? ')
-
-			stroke_color = self.stroke_color(fill_color)
-			fill_color = self.fill_color(fill_color)
-
-			start = '<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body bgcolor=\"' + \
-					str(fill_color)+'\">\n'
-			resolution = '<svg width=\"' + \
-				str(w)+'\" height=\"' + str(h) + '\">\n'
-			end = '</svg>\n</body>\n</html>\n'
 
 			if ask_edge_point == 'y':
 				ask_edge_dis = input('MM or Pixel (mm or pixel) ? ')
@@ -68,21 +64,33 @@ class luminance(object):
 					edge = float(input('Edge (mm) ? ')) / pixel_pitch
 				if ask_edge_dis == 'pixel':
 					edge = float(input('Edge (pixel) ? '))
-				body_edge = self.calculator_edge(
-					w, h, edge, solid_radius, stroke_color, fill_color)
-				fp = open('luminance_edge.html', 'w')
+
+			for fill_color in range(1, 6):
+				stroke_color = self.stroke_color(fill_color)
+				fill_color = self.fill_color(fill_color)
+
+				start = '<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body bgcolor=\"' + \
+						str(fill_color)+'\">\n'
+				resolution = '<svg width=\"' + \
+					str(w)+'\" height=\"' + str(h) + '\">\n'
+				end = '</svg>\n</body>\n</html>\n'
+
+				if ask_edge_point == 'y':
+					body_edge = self.calculator_edge(
+						w, h, edge, solid_radius, stroke_color, fill_color)
+					fp = open(fill_color + '_edge.html', 'w')
+					fp.write(start)
+					fp.write(resolution)
+					fp.write(body_edge)
+					fp.write(end)
+
+				body = self.calculator(w, h, aliquots, point,
+									   solid_radius, stroke_color, fill_color)
+				fp = open(fill_color + '.html', 'w')
 				fp.write(start)
 				fp.write(resolution)
-				fp.write(body_edge)
+				fp.write(body)
 				fp.write(end)
-
-			body = self.calculator(w, h, aliquots, point,
-								   solid_radius, stroke_color, fill_color)
-			fp = open('luminance.html', 'w')
-			fp.write(start)
-			fp.write(resolution)
-			fp.write(body)
-			fp.write(end)
 		except Exception as e:
 			raise e
 
@@ -175,9 +183,21 @@ class luminance(object):
 		except Exception as e:
 			raise e
 
-	def htmltoimage(self, value):
-		if os.path.isfile(value + '.html'):
-			imgkit.from_file(value + '.html', value + '.jpg')
+	def htmltoimage(self, fill_color):
+		if os.path.isfile(self.fill_color(fill_color) + '.html'):
+			imgkit.from_file(self.fill_color(fill_color) +
+							 '.html', self.fill_color(fill_color) + '.jpg')
+			os.system('rename ' + self.fill_color(fill_color) +
+					  '.jpg ' + self.fill_color_rename(fill_color) + '.jpg')
+		else:
+			pass
+
+	def htmltoimage_edge(self, fill_color):
+		if os.path.isfile(self.fill_color(fill_color) + '_edge.html'):
+			imgkit.from_file(self.fill_color(
+				fill_color) + '_edge.html', self.fill_color(fill_color) + '_edge.jpg')
+			os.system('rename ' + self.fill_color(fill_color) +
+					  '_edge.jpg ' + self.fill_color_rename(fill_color) + '_edge.jpg')
 		else:
 			pass
 
@@ -187,6 +207,7 @@ if __name__ == '__main__':
 	os.system('del /f /q *.jpg')
 	luminance = luminance()
 	luminance.initial()
-	luminance.htmltoimage('luminance')
-	luminance.htmltoimage('luminance_edge')
+	for fill_color in range(1, 6):
+		luminance.htmltoimage(fill_color)
+		luminance.htmltoimage_edge(fill_color)
 	os.system('del /f /q *.html')
