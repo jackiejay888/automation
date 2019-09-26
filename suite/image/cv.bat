d03557e07daf2a81adbb4934b74291ea33dbc699
ZL Chen <ZL.Chen@advantech.com.tw>
2019-09-26 18:22:43 +0800

    update the suite folder

--- suite/image/cv.bat
+++
@@ -1,4 +0,0 @@
-D:
-cd D:/code/automation/suite/image/
-python cv_compare.pyc
-shutdown /r /t 5
--- suite/resolution/format.bat
+++
@@ -1,9 +0,0 @@
-del /f /q resolution.txt
-
-del /f /q counter.txt
-
-cd. > resolution.txt 
-
-cd. > counter.txt
-
-echo 1 > counter.txt
