f15c28f7cd0bd2bd21fcbc8a6528e5313d85d334
ZL Chen <ZL.Chen@advantech.com.tw>
2019-10-09 09:17:56 +0800

    commit the program_index.py

---
+++ suite/windows/program_index/program_index.py
@@ -0,0 +1,15 @@
+'''
+Created on 2019/10/14
+
+@Title: Coding the ProgramIndex support to the test tool edition update.
+@Author: ZL Chen
+'''
+
+class program_index(object):
+
+	def index(self):
+		pass
+
+if __name__ == '__main__':
+	pi = program_index()
+	pi.index()
