8c01ae357162720271462455581ecf74f701cc41
ZL Chen <ZL.Chen@advantech.com.tw>
2019-10-24 11:09:08 +0800

    Add tensorflow folder

---
+++ suite/windows/tensorflow/tensorflow.py
@@ -0,0 +1,15 @@
+'''
+Created on 2019/10/24
+
+@Title: Coding the GPU performance by tensorflow lib base on python.
+@Author: ZL Chen
+'''
+
+class tensorflow(object):
+
+	def initial(self):
+		pass
+
+if __name__ == '__main__':
+	tensorflow = tensorflow()
+	tensorflow.initial()
