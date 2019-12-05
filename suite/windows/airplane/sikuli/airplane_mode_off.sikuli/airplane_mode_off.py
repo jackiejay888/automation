ccbad7ae6d34521dcad05e5f44ca59b04bb4aa40
ZL Chen <ZL.Chen@advantech.com.tw>
2019-12-05 15:29:39 +0800

    Add the sikuli for airplane windows on/off

---
+++ suite/windows/airplane/sikuli/airplane_mode_off.sikuli/airplane_mode_off.py
@@ -0,0 +1,16 @@
+if exists("1575513389011.png"):
+    click("1575513389011.png")
+    if exists("1575513389011.png"):
+        click("1575513389011.png")
+        if exists("1575513496270.png"):
+            click("1575513496270.png")
+        if exists("Airplanemode.png"):
+            click("Airplanemode.png")
+    else:
+        raise
+else:
+    raise
+if exists("1575523240030.png"):
+    click("1575523240030.png")
+else:
+    raise
---
+++ suite/windows/airplane/sikuli/airplane_mode_on.sikuli/airplane_mode_on.py
@@ -0,0 +1,15 @@
+if exists("1575511619780.png"):
+    click("1575511619780.png")
+    if exists("1575511825969.png"):
+        click("1575511825969.png")
+    else:
+        raise
+else:
+    if exists("1575514366004.png"):
+        click("1575514366004.png")
+        if exists("1575511825969.png"):
+            click("1575511825969.png")
+        if exists("Airplanemode.png"):
+            click("Airplanemode.png")
+    else:
+        raise
