b605dcf83696a8f28f9967742f39dae090ce23b7
ZL Chen <ZL.Chen@advantech.com.tw>
2019-12-11 18:01:46 +0800

    commit the sikuli for sleep(1) script

--- suite/windows/airplane/sikuli/airplane_mode_off.sikuli/airplane_mode_off.py
+++ suite/windows/airplane/sikuli/airplane_mode_off.sikuli/airplane_mode_off.py
@@ -1,16 +1,15 @@
 if exists("1575513389011.png"):
+    wait(1)
     click("1575513389011.png")
     if exists("1575513389011.png"):
+        wait(1)
         click("1575513389011.png")
         if exists("1575513496270.png"):
+            wait(1)
             click("1575513496270.png")
         if exists("Airplanemode.png"):
+            wait(1)
             click("Airplanemode.png")
-    else:
-        raise
-else:
-    raise
 if exists("1575523240030.png"):
+    wait(1)
     click("1575523240030.png")
-else:
-    raise
--- suite/windows/airplane/sikuli/airplane_mode_on.sikuli/airplane_mode_on.py
+++ suite/windows/airplane/sikuli/airplane_mode_on.sikuli/airplane_mode_on.py
@@ -1,15 +1,16 @@
 if exists("1575511619780.png"):
+    wait(1)
     click("1575511619780.png")
     if exists("1575511825969.png"):
+        wait(1)
         click("1575511825969.png")
-    else:
-        raise
 else:
     if exists("1575514366004.png"):
+        wait(1)
         click("1575514366004.png")
         if exists("1575511825969.png"):
+            wait(1)
             click("1575511825969.png")
         if exists("Airplanemode.png"):
+            wait(1)
             click("Airplanemode.png")
-    else:
-        raise
