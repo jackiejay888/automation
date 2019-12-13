3d4ba76a9ff5108c2b206dde53dc76969173223c
ZL Chen <ZL.Chen@advantech.com.tw>
2019-12-13 15:57:06 +0800

    Update the sikuli api

--- suite/windows/airplane/sikuli/airplane_mode_off.sikuli/airplane_mode_off.py
+++ suite/windows/airplane/sikuli/airplane_mode_off.sikuli/airplane_mode_off.py
@@ -6,10 +6,12 @@ if exists("1575513389011.png"):
         wait(1)
         if exists("1575513496270.png"):
             click("1575513496270.png")
-            wait(5)
+            wait(10)
         if exists("Airplanemode.png"):
             click("Airplanemode.png")
-            wait(5)
+            wait(10)
 if exists("1575523240030.png"):
+    click("1575523240030.png")
+    wait(1)
     click("1575523240030.png")
     wait(1)
