56e4a9aea2ecc396a138e9a7c1cf2ab56f424e99
ZL Chen <ZL.Chen@advantech.com.tw>
2019-12-17 18:20:23 +0800

    commit the burnintest code

---
+++ suite/windows/burnintest/delele.bat
@@ -0,0 +1,82 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCuDJHuR5kwZJi9RXxC+Pn6oCLwS/O3o+9aEoEcFXq8uLofeg/necLFd40brFQ==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJHuR5kwZJi9RXxC+Pn6oCLwS/O3o+9aDp0EYQOdxfZfeug==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+@echo off
+CLS
+ECHO.
+ECHO =============================
+ECHO Running Admin Shell
+ECHO =============================
+
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO **************************************
+ECHO Invoking UAC for Privilege Escalation
+ECHO **************************************
+
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+::::::::::::::::::::::::::::
+::START
+::::::::::::::::::::::::::::
+
+del /q C:\*BIT_Log_*.htm C:\*BIT_Log_*.log
+
+timeout 3
---
+++ suite/windows/burnintest/execute.bat
@@ -0,0 +1,54 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFBpQQQ2MAE+/Fb4I5/jH7OiNukdQRbQ3fdaWiuTfbukQ5SU=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCuDJHuR5kwZJi9RXxC+Pn6oCLwS/O3o+9aCukgeQfY6R9iJirmHLq4W8kCE
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+..\bit.exe -c dqa.bitcfg -r -p -D 720
+
+timeout 5
+
+@echo off
+
+set nowYear=%date:~10,4%
+set nowMonth=%date:~4,2%
+set nowDay=%date:~7,2%
+set nowHour=%time:~0,2%
+set nowHour=%nowHour: =0%
+set nowMinute=%time:~3,2%
+set nowSecond=%time:~6,2%
+set datetime=%nowYear%%nowMonth%%nowDay%_%nowHour%%nowMinute%%nowSecond%
+
+md %datetime%
+
+xcopy /y C:\*BIT_log_*.htm %datetime%
+xcopy /y C:\*BIT_log_*.log %datetime%
+
+move %datetime% ./backup/
