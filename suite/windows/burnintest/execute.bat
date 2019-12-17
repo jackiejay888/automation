82e4354eca6c9501cb563c2be094735afafee7d3
ZL Chen <ZL.Chen@advantech.com.tw>
2019-12-17 21:33:07 +0800

    update the burnintest

--- suite/windows/burnintest/execute.bat
+++ suite/windows/burnintest/execute.bat
@@ -1,36 +1,3 @@
-::[Bat To Exe Converter]
-::
-::YAwzoRdxOk+EWAjk
-::fBw5plQjdCyDJGyX8VAjFBpQQQ2MAE+/Fb4I5/jH7OiNukdQRbQ3fdaWiuTfbukQ5SU=
-::YAwzuBVtJxjWCl3EqQJgSA==
-::ZR4luwNxJguZRRnk
-::Yhs/ulQjdF+5
-::cxAkpRVqdFKZSDk=
-::cBs/ulQjdF+5
-::ZR41oxFsdFKZSDk=
-::eBoioBt6dFKZSDk=
-::cRo6pxp7LAbNWATEpCI=
-::egkzugNsPRvcWATEpCI=
-::dAsiuh18IRvcCxnZtBJQ
-::cRYluBh/LU+EWAnk
-::YxY4rhs+aU+JeA==
-::cxY6rQJ7JhzQF1fEqQJQ
-::ZQ05rAF9IBncCkqN+0xwdVs0
-::ZQ05rAF9IAHYFVzEqQJQ
-::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
-::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
-::cRolqwZ3JBvQF1fEqQJQ
-::dhA7uBVwLU+EWDk=
-::YQ03rBFzNR3SWATElA==
-::dhAmsQZ3MwfNWATElA==
-::ZQ0/vhVqMQ3MEVWAtB9wSA==
-::Zg8zqx1/OA3MEVWAtB9wSA==
-::dhA7pRFwIByZRRnk
-::Zh4grVQjdCuDJHuR5kwZJi9RXxC+Pn6oCLwS/O3o+9aCukgeQfY6R9iJirmHLq4W8kCE
-::YB416Ek+ZG8=
-::
-::
-::978f952a14a936cc963da21a135fa983
 ..\bit.exe -c dqa.bitcfg -r -p -D 720
 
 timeout 5
