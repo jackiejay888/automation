55484d60d9134495feb1901e65895de43de73e46
ZL Chen <ZL.Chen@advantech.com.tw>
2019-12-30 13:59:04 +0800

    commit sikuli code

--- suite/windows/airplane/sikuli/airplane_mode_on.sikuli/airplane_mode_on.py
+++ suite/windows/airplane/sikuli/airplane_mode_on.sikuli/airplane_mode_on.py
@@ -14,3 +14,23 @@ else:
         if exists("Airplanemode.png"):
             click("Airplanemode.png")
             wait(2)
+    else:
+        if exists("1577260684376.png"):
+            click("1577260684376.png")
+            wait(1)
+            if exists("1575511825969.png"):
+                click("1575511825969.png")
+                wait(2)
+            if exists("Airplanemode.png"):
+                click("Airplanemode.png")
+                wait(2)
+        else:
+            if exists("1577345319202.png"):
+                click("1577345319202.png")
+                wait(1)
+                if exists("1575511825969.png"):
+                    click("1575511825969.png")
+                    wait(2)
+                if exists("Airplanemode.png"):
+                    click("Airplanemode.png")
+                    wait(2)
