521017a602ebac3abfc5a495312d5e90ba439578
ZL Chen <ZL.Chen@advantech.com.tw>
2019-12-31 17:24:35 +0800

    update kpi and program index

--- suite/windows/program_index/app.py
+++ suite/windows/program_index/app.py
@@ -7,59 +7,38 @@ Created on 2019/11/19
 
 import os
 import sys
-import sqlite3
-from PyQt5.QtWidgets import QDialog, QApplication, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QApplication
 from app_program_index import Ui_Form
 from ftplib import FTP
+
 
 class AppWindow(QDialog):
 
 	def __init__(self):
 		super().__init__()
 		self.ui = Ui_Form()
-		self.ui.setupUi(self)		
-
+		self.ui.setupUi(self)
 		#--------------------------------#
 		self.ui.pushButton_3.clicked.connect(self.search)
-
-		#--------------------------------#
-		self.ui.pushButton_2.clicked.connect(self.download_to)
-
-		#--------------------------------#
 		self.show()
 
-	def download_to(self):
-		# self.ui.lineEdit.setText(_translate(self, '11111'))
-		pass
-
 	def search(self):
-		self.case_folder()
 		ip = self.ui.lineEdit_2.text()
 		ftp = FTP(ip)
-		# ip = '172.17.9.225'
-		# ftp.login('dqa','dqa8101d')
-		ftp.login('admin','admin')
+		ftp.login('admin', 'admin')
 		ftp.retrlines('LIST')
-
-		# self.case_folder()
-
+		self.case_folder()
 
 	def case_folder(self):
 		if self.ui.comboBox_2.currentText() != '':
 			self.ui.comboBox.setEditable(True)
 		else:
-			self.messagebox_warning()
+			self.ui.comboBox.setEditable(False)
 
-	def messagebox_warning(self):
-		msg = QMessageBox()
-		msg.setIcon(QMessageBox.Information)
-		msg.setText("This is a message box")
-		msg.setInformativeText("This is additional information")
-		msg.setWindowTitle('suite folder')
-		msg.setDetailedText("The details are as follows:")
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	w = AppWindow()
-	w.show()
+	appwindow = AppWindow()
+	appwindow.show()
 	sys.exit(app.exec_())
