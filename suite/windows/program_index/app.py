2958ff12c16b1cd0c7ad5517dbd31dfe5e7aab2d
ZL Chen <ZL.Chen@advantech.com.tw>
2020-01-16 23:55:51 +0800

    Update the program index

--- suite/windows/program_index/app.py
+++ suite/windows/program_index/app.py
@@ -9,8 +9,8 @@ import os
 import sys
 import configparser
 # from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QDesktopWidget
-# from PyQt5.QtCore import QDir
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.QtWidgets import QDesktopWidget, QMessageBox, QListWidget
 from app_program_index import Ui_Form_dqa
 from ftplib import FTP
 
@@ -29,21 +29,31 @@ class AppWindow(QDialog):
 		self.ui.pushButton_search.clicked.connect(self.search)
 		#---------------------------------------------------------------------#
 		self.ui.lineEdit_open.setText(os.getcwd())
-		self.ui.comboBox_suite.setItemText(0, config.get('set', 'suite_name'))
+
+		#---------------------------------------------------------------------#	
+		self.ui.comboBox_suite.setItemText(0, config.get('set', 'suite_name'))	
+		self.ui.comboBox_suite.highlighted.connect(self.suite_folder)
+		#---------------------------------------------------------------------#
 		self.ui.pushButton_open.clicked.connect(self.open)
+		#---------------------------------------------------------------------#
+		# self.ui.listwidget_view.itemClicked.connect(self.view)
 		#---------------------------------------------------------------------#
 		self.ui.pushButton_close.clicked.connect(self.close)
 		#---------------------------------------------------------------------#
 		self.center()
 		self.show()
 
-	def search(self):
-		ip = self.ui.lineEdit_ip.text()
-		ftp = FTP(ip)
-		ftp.login(config.get('set', 'username'), config.get('set', 'password'))
-		ftp.cwd('/' + str(self.ui.comboBox_suite.currentText()))
-		print(ftp.retrlines('LIST'))
-		self.case_folder()
+	def center(self):
+		screen = QDesktopWidget().screenGeometry()
+		size = self.geometry()
+		self.move((screen.width() - size.width()) / 2,
+				  (screen.height() - size.height()) / 2)
+
+	def suite_folder(self):
+		self.ui.comboBox_case.setEditable(True)
+
+	def case_folder(self):
+		pass
 
 	def open(self):
 		dialog = QFileDialog()
@@ -52,17 +62,17 @@ class AppWindow(QDialog):
 			for dirname in dialog.selectedFiles():
 				self.ui.lineEdit_open.setText(str(dirname))
 
-	def case_folder(self):
-		if self.ui.comboBox_suite.currentText() != '':
-			self.ui.comboBox_case.setEditable(True)
-		else:
-			self.ui.comboBox_case.setEditable(False)
+	def view(self):
+		msg = QMessageBox()
+		msg.information('error')
+		pass
 
-	def center(self):
-		screen = QDesktopWidget().screenGeometry()
-		size = self.geometry()
-		self.move((screen.width() - size.width()) / 2,
-				  (screen.height() - size.height()) / 2)
+	def search(self):
+		ip = self.ui.lineEdit_ip.text()
+		ftp = FTP(ip)
+		ftp.login(config.get('set', 'username'), config.get('set', 'password'))
+		ftp.cwd('/' + str(self.ui.comboBox_suite.currentText()))
+		print(ftp.retrlines('LIST'))
 
 
 if __name__ == '__main__':
