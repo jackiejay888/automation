c29fabbc810f706652a4d86d2a9ce8efc42d2f68
ZL <jackiejay888@gmail.com>
2020-01-17 00:04:39 +0800

    Update the program index code

--- suite/windows/program_index/app.py
+++ suite/windows/program_index/app.py
@@ -24,30 +24,64 @@ class AppWindow(QDialog):
 		super().__init__()
 		self.ui = Ui_Form_dqa()
 		self.ui.setupUi(self)
-		#---------------------------------------------------------------------#
+		# Initial the servier ip address
 		self.ui.lineEdit_ip.setText(config.get('set', 'server_ip'))
-		self.ui.pushButton_search.clicked.connect(self.search)
-		#---------------------------------------------------------------------#
-		self.ui.lineEdit_open.setText(os.getcwd())
 
-		#---------------------------------------------------------------------#	
-		self.ui.comboBox_suite.setItemText(0, config.get('set', 'suite_name'))	
+
+
+		# Signal the search button 
+		self.ui.pushButton_search.clicked.connect(self.search)
+
+
+
+
+
+		# Initial the program index's root directory
+		self.ui.lineEdit_open.setText(os.getcwd())
+		# Initial the suite folder value ('')
+		self.ui.comboBox_suite.setItemText(0, config.get('set', 'suite_name'))
+		# Highlight the select value
 		self.ui.comboBox_suite.highlighted.connect(self.suite_folder)
-		#---------------------------------------------------------------------#
+
+
+
+
+
+		# Initial the listwidget view
+		self.ui.listwidget_view.itemClicked.connect(self.view)
+
+
+
+
+
+
+
+
+
+
+
+
+		# Signal the open button
 		self.ui.pushButton_open.clicked.connect(self.open)
-		#---------------------------------------------------------------------#
-		# self.ui.listwidget_view.itemClicked.connect(self.view)
-		#---------------------------------------------------------------------#
+		# Slot the close button
 		self.ui.pushButton_close.clicked.connect(self.close)
-		#---------------------------------------------------------------------#
+		# Setup the windows to center
 		self.center()
 		self.show()
 
-	def center(self):
-		screen = QDesktopWidget().screenGeometry()
-		size = self.geometry()
-		self.move((screen.width() - size.width()) / 2,
-				  (screen.height() - size.height()) / 2)
+
+	def search(self):
+		ip = self.ui.lineEdit_ip.text()
+		ftp = FTP(ip)
+		ftp.login(config.get('set', 'username'), config.get('set', 'password'))
+		ftp.cwd('/' + str(self.ui.comboBox_suite.currentText()))
+		print(ftp.retrlines('LIST'))
+
+
+	def view(self):
+		msg = QMessageBox()
+		msg.information('error')
+		pass
 
 	def suite_folder(self):
 		self.ui.comboBox_case.setEditable(True)
@@ -62,18 +96,11 @@ class AppWindow(QDialog):
 			for dirname in dialog.selectedFiles():
 				self.ui.lineEdit_open.setText(str(dirname))
 
-	def view(self):
-		msg = QMessageBox()
-		msg.information('error')
-		pass
-
-	def search(self):
-		ip = self.ui.lineEdit_ip.text()
-		ftp = FTP(ip)
-		ftp.login(config.get('set', 'username'), config.get('set', 'password'))
-		ftp.cwd('/' + str(self.ui.comboBox_suite.currentText()))
-		print(ftp.retrlines('LIST'))
-
+	def center(self):
+		screen = QDesktopWidget().screenGeometry()
+		size = self.geometry()
+		self.move((screen.width() - size.width()) / 2,
+				  (screen.height() - size.height()) / 2)
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
