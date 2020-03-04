d3b7e11bdd6d5c4d6ff0b17132a6ed0ad73cc7b7
ZL Chen <ZL.Chen@advantech.com.tw>
2020-03-04 16:58:54 +0800

    Add click_button code

--- suite/windows/click_button/app.py
+++ suite/windows/click_button/app.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from click_button import Ui_Form_click_button
 '''
 Created on 2020/03/03
 
@@ -12,15 +11,16 @@ Created on 2020/03/03
 import os
 import sys
 import time
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QDialog
-from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QFileDialog
-from PyQt5.QtWidgets import QDesktopWidget
-from PyQt5.QtWidgets import QListWidget
-from PyQt5.QtWidgets import QMessageBox
-
-os.system('pyuic5 click_button.ui > click_button.py')
+from PyQt5 import QtCore, \
+					QtGui, \
+					QtWidgets
+from PyQt5.QtWidgets import QDialog, \
+							QApplication, \
+							QFileDialog, \
+							QDesktopWidget, \
+							QListWidget, \
+							QMessageBox
+from click_button import Ui_Form_click_button
 
 
 class AppWindow(QDialog):
@@ -32,17 +32,40 @@ class AppWindow(QDialog):
 
 		self.ui.setupUi(self)
 
+		# Initial the match.txt timer
+		self.initial_match_timer()
+
+		# Show the lcd_show value
+		read_timer = open('timer.txt')
+		self.ui.lcd_show.setProperty("intValue", int(read_timer.read()))
+
 		# Signal the click_button button
 		self.ui.click_button.clicked.connect(self.timer)
 
 		# Signal the log_reset button
 		self.ui.log_reset.clicked.connect(self.reset)
 
-		self.windows_center()  # Setup the windows to center
+		# Windows initial setting
+		self.windows_setting()  # Setup the windows to center
 
 		self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)
 
 		self.show()
+
+	def initial_match_timer(self):
+		read_timer = open('timer.txt', 'r')
+		try:
+			read_timer_add = int(str(read_timer.read())) + 1
+			print(str(read_timer_add))
+			read_timer_value = int(read_timer_add)
+		finally:
+			read_timer.close()
+
+		write_timer = open('match.txt', 'w')
+		try:
+			write_timer.write(str(read_timer_value))
+		finally:
+			write_timer.close()		
 
 	def reset(self):
 		timer = 0
@@ -55,12 +78,12 @@ class AppWindow(QDialog):
 			write_timer.close()
 		read_timer = open('timer.txt')
 		try:
-			self.ui.click_button.setText(read_timer.read())
-			pass
+			self.ui.lcd_show.setProperty("intValue", int(read_timer.read()))
 		finally:
 			read_timer.close()
+		self.initial_match_timer()
 		MessageBox = QMessageBox()
-		MessageBox.information(self, 'Clear the log',
+		MessageBox.information(self, 'CLEAR',
 							   'Clear the log is completed.')
 
 	def timer(self):
@@ -74,19 +97,31 @@ class AppWindow(QDialog):
 			write_timer.close()
 		read_timer = open('timer.txt')
 		try:
-			self.ui.click_button.setText(read_timer.read())
-			pass
+			self.ui.lcd_show.setProperty("intValue", int(read_timer.read()))
+			if open('match.txt', 'r').read() == open('timer.txt', 'r').read():
+				print('PASS')
+				os.system('shutdown /s /t 0')
+			else:
+				print('FAIL')
 		finally:
 			read_timer.close()
 
-	def windows_center(self):
+	def windows_setting(self):
 		screen = QDesktopWidget().screenGeometry()
 		size = self.geometry()
 		self.resize(screen.width(), screen.height())
 		self.ui.click_button.setGeometry(
 			QtCore.QRect(0, 0, screen.width(), screen.height()))
-		self.ui.log_reset.setGeometry(
-			QtCore.QRect(screen.width()/2-25, 0, 50, 50))
+		lcd_show_size_x = 400
+		lcd_show_size_y = 180
+		self.ui.lcd_show.setGeometry(QtCore.QRect(screen.width() / 2 - lcd_show_size_x / 2, \
+			(screen.height() / 2 - lcd_show_size_y / 2) - lcd_show_size_y, lcd_show_size_x, lcd_show_size_y))
+		log_reset_size_x = 400
+		log_reset_size_y = 60
+		self.ui.log_reset.setGeometry(QtCore.QRect(screen.width() / 2 - log_reset_size_x / 2, \
+			log_reset_size_y / 2, log_reset_size_x, log_reset_size_y))
+		font = QtGui.QFont()
+		font.setPointSize(80)
 
 
 if __name__ == '__main__':
