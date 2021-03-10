b27310fc377745dab852669fab0b98acb208fe9b
jackiejay888 <jaciejay888@gmail.com>
2021-03-10 14:22:30 +0800

    Modify the auto.py

--- appium/auto.py
+++ appium/auto.py
@@ -5,5 +5,7 @@ ZL
 import os
 
 os.system('python calculator_a8_a9.py')
-os.system('python language_change.py')
-os.system('python clock_app.py')
+# os.system('python calculator_a10.py')
+os.system('python language_change_a8_a9.py')
+# os.system('python clock_app_a8.py')
+os.system('python clock_app_a9.py')
