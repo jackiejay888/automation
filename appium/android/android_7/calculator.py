32a8da11666aecc8c558ac4e48f0dfdca9bd85a9
jackiejay888 <jaciejay888@gmail.com>
2021-03-18 13:32:40 +0800

    Add the autotest script of android 7

---
+++ appium/android/android_7/calculator.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+from appium import webdriver
+from nose.tools import *
+# import unittest
+# import HtmlTestRunner
+from HTMLTestRunner import *
+
+class take_screen_shot():
+	def __init__(self, func):
+		self.func = func
+		self.name = func.__name__ + ' (__main__.CalTestCase).png'
+	def __call__(self, *args):
+		try:
+			self.func(self, *args)
+		finally:
+			driver.get_screenshot_as_file(self.name)
+
+class CalTestCase(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		desired_caps = {}
+		desired_caps['platformName'] = 'Android'
+		desired_caps['platformVersion'] = '7.1.2'
+		desired_caps['deviceName'] = 'rk3399_rsb4710'
+		desired_caps['appPackage'] = 'com.android.calculator2'
+		desired_caps['appActivity'] = '.Calculator'
+		global driver
+		driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+	@classmethod
+	def tearDownClass(self):
+		driver.quit()
+		
+	# @take_screen_shot
+	# def test_case_01_open(self):
+	#     text = driver.find_element_by_id('com.android.calculator2:id/formula').text
+	#     assert_equal(text,"")
+
+	@take_screen_shot
+	def test_case_01_plus(self):
+		driver.find_element_by_id('com.android.calculator2:id/digit_1').click()
+		driver.find_element_by_id('com.android.calculator2:id/op_add').click()
+		driver.find_element_by_id('com.android.calculator2:id/digit_2').click()
+		driver.find_element_by_id('com.android.calculator2:id/eq').click()
+		text = driver.find_element_by_xpath('//*[@resource-id="com.android.calculator2:id/result"]').text
+		assert_equal(text,"3")
+		
+	@take_screen_shot
+	def test_case_02_sub(self):
+		driver.find_element_by_id('com.android.calculator2:id/digit_5').click()
+		driver.find_element_by_id('com.android.calculator2:id/op_sub').click()
+		driver.find_element_by_id('com.android.calculator2:id/digit_3').click()
+		driver.find_element_by_id('com.android.calculator2:id/eq').click()
+		text = driver.find_element_by_xpath('//*[@resource-id="com.android.calculator2:id/result"]').text
+		assert_equal(text,"2")
+
+	@take_screen_shot
+	def test_case_03_mul(self):
+		driver.find_element_by_id('com.android.calculator2:id/digit_2').click()
+		driver.find_element_by_id('com.android.calculator2:id/op_mul').click()
+		driver.find_element_by_id('com.android.calculator2:id/digit_3').click()
+		driver.find_element_by_id('com.android.calculator2:id/eq').click()
+		text = driver.find_element_by_xpath('//*[@resource-id="com.android.calculator2:id/result"]').text
+		assert_equal(text,"6")
+
+	@take_screen_shot
+	def test_case_04_div(self):
+		driver.find_element_by_id('com.android.calculator2:id/digit_9').click()
+		driver.find_element_by_id('com.android.calculator2:id/op_div').click()
+		driver.find_element_by_id('com.android.calculator2:id/digit_3').click()
+		driver.find_element_by_id('com.android.calculator2:id/eq').click()
+		text = driver.find_element_by_xpath('//*[@resource-id="com.android.calculator2:id/result"]').text
+		assert_equal(text,"3")
+
+if __name__ == '__main__':
+	# unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='report',
+	#                                                        report_title='Android计算器测试报告',encoding='utf-8'))
+	report_dir=r'calculatorTest.html'
+	re_open=open(report_dir,'wb')
+	suite=unittest.TestLoader().loadTestsFromTestCase(CalTestCase)
+	runner=HTMLTestRunner(
+						stream=re_open,
+						title=u'Android 计算器自动化',
+						description=u'测试报告')
+	runner.run(suite)
---
+++ appium/android/android_7/languagechange.py
@@ -0,0 +1,99 @@
+from appium import webdriver
+from nose.tools import *
+from HTMLTestRunner import *
+from appium.webdriver.common.touch_action import TouchAction
+import os, time, uiautomator2
+
+class take_screen_shot():
+	def __init__(self, func):
+		self.func = func
+		self.name = func.__name__ + '.png'
+	def __call__(self, *args):
+		try:
+			self.func(self, *args)
+		finally:
+			driver.get_screenshot_as_file(self.name)
+
+class Language(unittest.TestCase):
+	@classmethod
+	def setUpClass(self):
+		desired_caps = {}
+		desired_caps['platformName'] = 'Android'
+		desired_caps['platformVersion'] = '7.1.2'
+		desired_caps['deviceName'] = 'rk3399_rsb4710'
+		desired_caps['appPackage'] = 'com.android.settings'
+		desired_caps['appActivity'] = 'com.android.settings.Settings'
+		global driver
+		driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+
+	@classmethod
+	def tearDownClass(self):
+		driver.quit()
+
+	@take_screen_shot
+	def test_case_01_Chinese(self):
+		width = driver.get_window_size()['width']
+		height = driver.get_window_size()['height']
+		i = 0
+		while i < 10:
+			try:
+				driver.find_element_by_xpath('//*[@text="Languages & input"]').click()
+				break
+			except Exception as e:
+				driver.swipe(width / 2, height * 0.8, width / 2, height * 0.2)
+				i = i + 1
+		time.sleep(2)
+		driver.find_element_by_xpath('//*[@text="Languages"]').click()
+		time.sleep(2)
+		driver.find_element_by_xpath('//*[@resource-id="com.android.settings:id/add_language"]').click()
+		time.sleep(2)
+		p = 0
+		while p < 30:
+			try:
+				driver.find_element_by_xpath('//*[@text="简体中文"]').click()
+				break
+			except Exception as e:
+				driver.swipe(width / 2, height * 0.9, width / 2, height * 0.1)
+				p = p + 1
+		time.sleep(1)
+		driver.find_element_by_xpath('//*[@text="中国"]').click()
+		time.sleep(2)
+		el_0 = driver.find_element_by_xpath('//*[@resource-id="com.android.settings:id/dragList"]/android.widget.RelativeLayout[1]/android.widget.ImageView[1]')
+		el_1 = driver.find_element_by_xpath('//*[@resource-id="com.android.settings:id/add_language"]')
+		driver.scroll(el_0,el_1)
+		time.sleep(2)
+		test = driver.find_element_by_id('com.android.settings:id/add_language').text
+		assert_equal(test,'添加语言')
+
+	@take_screen_shot
+	def test_case_02_English(self):
+		# os.popen('adb shell am start com.android.settings/.Settings')
+		# width = driver.get_window_size()['width']
+		# height = driver.get_window_size()['height']
+		# i = 0
+		# while i < 10:
+		#     try:
+		#         driver.find_element_by_xpath('//*[@text="语言和输入法"]').click()
+		#         break
+		#     except Exception as e:
+		#         driver.swipe(width / 2, height * 0.8, width / 2, height * 0.2)
+		#         i = i + 1
+		# time.sleep(2)
+		# driver.find_element_by_xpath('//*[@text="语言"]').click()
+		# time.sleep(2)
+		el_0 = driver.find_element_by_xpath('//*[@content-desc="1, 简体中文（中国）"]/android.widget.ImageView[1]')
+		el_1 = driver.find_element_by_xpath('//*[@resource-id="com.android.settings:id/add_language"]')
+		driver.scroll(el_0,el_1)
+		time.sleep(2)
+		test = driver.find_element_by_id('com.android.settings:id/add_language').text
+		assert_equal(test,'Add a language')
+
+if __name__ == '__main__':
+	report_dir=r'languagechange.html'
+	re_open=open(report_dir,'wb')
+	suite=unittest.TestLoader().loadTestsFromTestCase(Language)
+	runner=HTMLTestRunner(
+						stream=re_open,
+						title=u'Android系统语言切换测试',
+						description=u'测试报告')
+	runner.run(suite)
