7dcf91681cf03731ea7e2c2d807418687ae98766
jackiejay888 <jaciejay888@gmail.com>
2021-05-18 09:23:06 +0800

    Update the zl_ping_tool

--- suite/test/zl_ping_tool.bat
+++ suite/test/zl_ping_tool.bat
@@ -46,7 +46,8 @@ set log="zl_ping_tool.log"
 echo Ping for windows environemnt. >> %log%
 @REM echo Date,Time,Status >> %log%
 :top
-set nowDate=%date:~6,4%/%date:~0,2%/%date:~3,2%
+@REM set nowDate=%date:~6,4%/%date:~0,2%/%date:~3,2%
+set nowDate=%date:~0,20%
 set nowHour=%time:~0,2%
 set nowHour=%nowHour: =0%
 set nowMinute=%time:~3,2%
